273bb8951a3d1e50e6357df635cdb9bcf7262ed4
Khoi Bui <emquacau123472>
2019-11-07 22:50:32 -0500

    initial commit

---
+++ apps/web-portal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'iresa-web-portal-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'web-portal';
+}
