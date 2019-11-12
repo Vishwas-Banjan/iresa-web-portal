99d304a893a65bb9fb5f432b7faa6ccdd7c5f768
Khoi Bui <kbui8@uncc.edu>
2019-11-11 21:02:13 -0500

    Add external libraries

--- apps/web-portal/src/app/app.component.spec.ts
+++ apps/web-portal/src/app/app.component.spec.ts
@@ -1,13 +1,10 @@
-import { Component } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      imports: [HttpClientModule]
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
@@ -15,5 +12,20 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'web-portal'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('web-portal');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to web-portal!'
+    );
   });
 });
